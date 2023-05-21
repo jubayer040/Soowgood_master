00a2c65457b2281509a28e9cac72ac2be36373aa
mojidul-islam <mojidul.i@coppanet.com>
2023-05-21 17:57:02 +0600

    s

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'dart:convert';
 import 'dart:developer';
 import 'dart:ui';
-
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
@@ -14,17 +13,17 @@ import 'package:soowgood/common/screen/splash_scree.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:soowgood/common/service/push_notification_service.dart';
 import 'package:soowgood/firebase_options.dart';
-
 import 'common/screen/notification_list_screen.dart'; //
 
-FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+    FlutterLocalNotificationsPlugin();
 
 //below function used when notification receive in background then this function initialise firebase
-Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async{
-  log('A Notifications msg show:  ${message}');
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  log('A Notifications msg show:  $message');
 }
 
-void main() async{
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   await FlutterDownloader.initialize(debug: true, ignoreSsl: true);
@@ -44,48 +43,37 @@ void main() async{
     FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
     return true;
   };
-  
-  runApp(MyApp());
+
+  runApp(const MyApp());
 }
 
-
-
 class MyApp extends StatelessWidget {
-  MyApp({Key? key}) : super(key: key);
-
-  Map<int, Color> color =
-  {
-    50: const Color.fromRGBO(48, 188, 237, 1.0),
-    100:const Color.fromRGBO(48, 188, 237, .2),
-    200:const Color.fromRGBO(48, 188, 237, .3),
-    300:const Color.fromRGBO(48, 188, 237, .4),
-    400:const Color.fromRGBO(48, 188, 237, .5),
-    500:const Color.fromRGBO(48, 188, 237, .6),
-    600:const Color.fromRGBO(48, 188, 237, .7),
-    700:const Color.fromRGBO(48, 188, 237, .8),
-    800:const Color.fromRGBO(48, 188, 237, .9),
-    900:const Color.fromRGBO(48, 188, 237, 1.0),
-  };
-
-
-  // This widget is the root of your application.
+  const MyApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-
+    Map<int, Color> color = {
+      50: const Color.fromRGBO(48, 188, 237, 1.0),
+      100: const Color.fromRGBO(48, 188, 237, .2),
+      200: const Color.fromRGBO(48, 188, 237, .3),
+      300: const Color.fromRGBO(48, 188, 237, .4),
+      400: const Color.fromRGBO(48, 188, 237, .5),
+      500: const Color.fromRGBO(48, 188, 237, .6),
+      600: const Color.fromRGBO(48, 188, 237, .7),
+      700: const Color.fromRGBO(48, 188, 237, .8),
+      800: const Color.fromRGBO(48, 188, 237, .9),
+      900: const Color.fromRGBO(48, 188, 237, 1.0),
+    };
     MaterialColor colorCustom = MaterialColor(0xFF30BCED, color);
     return GetMaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: MyString.soowgood,
-      theme: ThemeData(
-          primaryColor: MyColor.themeTealBlue,
-          primaryColorDark : MyColor.themeTealBlue,
-          primarySwatch: colorCustom
-      ),
+        debugShowCheckedModeBanner: false,
+        title: MyString.soowgood,
+        theme: ThemeData(
+            primaryColor: MyColor.themeTealBlue,
+            primaryColorDark: MyColor.themeTealBlue,
+            primarySwatch: colorCustom),
         home: const SplashScreen(),
         // initialRoute: AppRoutes.splashScreen,
         // getPages: AppPages.pages,
-        defaultTransition: Transition.rightToLeft
-    );
+        defaultTransition: Transition.rightToLeft);
   }
 }
-
